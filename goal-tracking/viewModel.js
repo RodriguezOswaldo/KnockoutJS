50c3572f27adbd62e8b67ce42c164e0db2da5966
rodriguezoswaldo <owner2303@gmail.com>
2020-08-11 11:37:57 -0400

    binding viewModel with view

--- goal-tracking/viewModel.js
+++ goal-tracking/viewModel.js
@@ -4,9 +4,18 @@ function getGoals(){
     });
 }
 
-function iewModel(){
+function ViewModel(){
     var m = this;
-    m.goalVs = ko.observableArray(['like', 'your', 'face']);
+    m.goals = ko.observableArray(['like', 'your', 'face']);
+    m.goalsInputName = ko.observable();
+    m.goalsInputType = ko.observable();
+    m.goalsInputDeadline = ko.observable();
+
+    m.addGoal = function(){
+        var name = $('#name').val();
+        var type = $('#type').val();
+        var deadline = $('#deadline').val();
+    }
 }
 var viewModel = new ViewModel();
 ko.applyBinding(viewModel);
